193d70a6ede0a83572adcaaeec86a00afd0efbc7
yahya <101sayyid@gmail.com>
2026-05-13 16:45:59 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+const themeToggle = document.getElementById("theme-toggle");
+const mainContent = document.querySelectorAll(".main-content");
+const body = document.querySelector("body");
+const icon = themeToggle.querySelector("i");
+const hamburger = document.getElementById("hamburger");
+const mobileMenu = document.getElementById("mobileMenu");
+const form = document.querySelector("contact-form");
+
+themeToggle.addEventListener("click", () => {
+  body.classList.toggle("light-mode");
+
+  if (body.classList.contains("light-mode")) {
+    icon.classList.remove("fa-moon");
+    icon.classList.add("fa-sun");
+  } else {
+    icon.classList.remove("fa-sun");
+    icon.classList.add("fa-moon");
+  }
+});
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  mobileMenu.classList.toggle("active");
+  mainContent.classList.toggle("blur");
+});
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+});
